fd0067d175bedfe310f4b68661d8049352c8e2fb
GithuiVictor <victorgithuim@gmail.com>
2022-08-07 13:54:38 +0300

    feat: Landing Page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import './footer.css'
+
+const socialMediaDetails = [
+  {
+    icon: "bx bxl-instagram-alt",
+    targetLink: "https://www.instagram.com/klein_nettoh/"
+  },
+  {
+    icon: "bx bxl-youtube",
+    targetLink: "https://www.youtube.com/channel/UCwIF3S_3rdaa9C-wp5QWOow"
+  }
+]
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='copyright'>
+        <p className="copyright_details">
+            {`Copyright © ${new Date().getFullYear()} Klein Nettoh. `}
+            {`Proudly created by `}
+            <a color="inherit" href="https://victor-githui.netlify.app/" target="_blank" rel="noreferrer">
+                Victor Githui
+            </a>{' '}
+            
+        </p>
+      </div>
+
+      <div className='socialmedia_wrapper'>
+        {
+          socialMediaDetails.map((socialMedia, index) => (
+            <a href={socialMedia.targetLink} target="_blank" key={index}>
+              <i className={socialMedia.icon}></i>
+            </a>
+            
+          ))
+        }
+        
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Navigation from '../../components/Navigation/Navigation'
+import mediaCategories from '../../assets/mediaCategories.json'
+import './home.css'
+import Footer from '../../components/Footer/Footer'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div  className="home">
         <Navigation />
+
+        <div className="home_content">
+          <div className='photography_categoryWrapper'>
+            {
+              mediaCategories &&
+              mediaCategories.map((mediaCategory, index) => (
+                <div 
+                  className={`photography_category ${mediaCategory.css_background}`} 
+                  key={index}
+                  onClick={() => navigate(mediaCategory.target)}
+                >
+                  <div className='categoryDetails_Wrapper' />
+                  <div className='category_details'>
+                    <h2>{mediaCategory.category}</h2>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+
+        <Footer />
     </div>
   )
 }
